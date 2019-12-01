0e11628b527dc5ef2078cca745fc61a54de93493
jnicaj <55360371+jnicaj@users.noreply.github.com>
2019-12-01 01:41:47 -0500

    added interest only and history of payments

--- LoanCalc/WindowsFormsApp1/ExistingLoan.cs
+++ LoanCalc/WindowsFormsApp1/ExistingLoan.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -24,22 +24,49 @@ namespace WindowsFormsApp1
         {
             amount = Convert.ToDouble(txtAmount.Text);
             interest = Convert.ToDouble(txtInterestRate.Text);
-            double time = Convert.ToDouble(txtMonths.Text);
+            double time = Convert.ToDouble(txtMonths.Text);           
             double final = monthlyPayment(amount, time, interest);
             string monthly = final.ToString("f2");
             txtEstimatedPayment.Text = monthly;
-
+            //displays monthly payment through principal with interest function
             double estimatedpayment = Convert.ToDouble(txtEstimatedPayment.Text);
             double principal = principalWithInterest(estimatedpayment, time);
             string principalpayment = principal.ToString("f2");
             txtPrincipal.Text = principalpayment;
+            double principalwithinterest = Convert.ToDouble(txtPrincipal.Text);
+            double interestOnly = InterestOnly(principalwithinterest, amount);
+            string interestValueOnly = interestOnly.ToString("C"); //currency
+            txtInterestOnly.Text = interestValueOnly;
+            //lists payments from initial payment month to end payment month
+            lstPayments.Items.Clear();
+            double principalRemaining = double.Parse(txtPrincipal.Text);
+            double principalPayment = double.Parse(txtEstimatedPayment.Text);
+            int p3 = int.Parse(txtMonths.Text);
+            double balance = principalRemaining;
+            for (int i = 1; i <= p3; i++)
+            {
+                balance = (balance)-(principalPayment);
+                lstPayments.Items.Add("Month " + " \t " + i.ToString() + " \t " +  balance.ToString("C")); //display each value of list with currency, loop
+            }
+             
+
         }
+
+        public static double InterestOnly (double initialAmount, double finalInterest)
+        {
+            double interestOnlyValue;
+            interestOnlyValue = initialAmount - finalInterest;
+            return (interestOnlyValue);
+
+        }
+
         public static double principalWithInterest(double monthlyValue, double loanDuration)
         {
             double Total;
             Total = (monthlyValue) * (loanDuration);
             return (Total);
         }
+
         public static double monthlyPayment(double existingAmount, double remainingMonths, double interestRate)
         {
             double rate, months, amount;
@@ -60,6 +87,8 @@ namespace WindowsFormsApp1
             txtInterestRate.Clear();
             txtMonths.Clear();
             txtPrincipal.Clear();
+            txtInterestOnly.Clear();
+            lstPayments.ResetText();
         }
 
         private void ExistingLoan_Load(object sender, EventArgs e)
