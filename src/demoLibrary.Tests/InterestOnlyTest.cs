1d446b5a046915a0cb1c52da9ac2e2300be3907b
jnicaj <55360371+jnicaj@users.noreply.github.com>
2019-12-11 20:42:39 -0500

    assignment7_JN

---
+++ src/demoLibrary.Tests/InterestOnlyTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using demoLibrary;
+using WindowsFormsApp1;
+using Xunit;
+
+namespace demoLibrary.Tests
+{
+    public class InterestOnlyTest
+    {
+        [Fact]
+        public void monthlyPayment_shouldGenerateCorrectMonthlyPayment()
+        {
+            //arrange
+            double InterestOnlyValue = 500.00;
+
+            //act
+            double InitialMinusFinal = ExistingLoan.InterestOnly(1000.00,500.00); 
+
+            //assert
+
+            Assert.Equal(InterestOnlyValue, InitialMinusFinal);
+        }
+    }
+}
---
+++ src/demoLibrary.Tests/PrincipalWithInterestTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using demoLibrary;
+using WindowsFormsApp1;
+using Xunit;
+
+namespace demoLibrary.Tests
+{
+    public class PrincipalWithInterestTest
+    {
+        [Fact]
+        public void principalWithInterest_shouldGenerateCorrectInterest()
+        {
+
+            //arrange
+            double TotalTestValue = 15060;
+
+            //act
+            double MonthlyTimesLoanDuration = ExistingLoan.principalWithInterest(251, 60);
+
+            //assert
+            Assert.Equal(TotalTestValue, MonthlyTimesLoanDuration);
+        }
+    }
+}
